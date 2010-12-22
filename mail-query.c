bbda5a92bf2864cba9b10a7b5465e98d3b1834bc
Dave Reisner <d@falconindy.com>
2010-12-21 21:56:23 -0500

    complete rewrite

--- mail-query.c
+++ mail-query.c
@@ -1,133 +1,163 @@
 #define _GNU_SOURCE
 
-#include <stdio.h>
-#include <string.h>
+#include <ctype.h>
 #include <dirent.h>
+#include <limits.h>
 #include <regex.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define REGEX_OPTS            REG_ICASE|REG_EXTENDED|REG_NOSUB|REG_NEWLINE
 
+typedef struct __address_t {
+  char *name;
+  char *email;
+} address_t;
+
 static regex_t regex;
-static char **entries = NULL;
-static size_t count = 0;
+static address_t **entries = NULL;
+static int entry_count = 0;
 
-/* parse the name and email from a string */
-void add_entries(char *str) {
-  char name[51];
-  char email[51];
-  char *entry;
+static const char *subdirs[] = { "new", "cur", NULL };
 
-  int rc = sscanf(str, "%50[^<]<%50[^>]", name, email);
+static int address_cmp(const void *a1, const void *a2) {
+  address_t *address1 = *(address_t**)a1;
+  address_t *address2 = *(address_t**)a2;
 
-  /* good parse, and search string found in email or name */
-  if (rc != 2) {
-    return;
-  }
-
-  /* if (strcasestr(email, search_string) || strcasestr(name, search_string)) */
-  if (regexec(&regex, name, 0, 0, 0) == REG_NOERROR ||
-      regexec(&regex, email, 0, 0, 0) == REG_NOERROR) {
-    asprintf(&entry, "%s\t%s", email, name);
-
-    /* allocate memory for the new entries */
-    if (!(entries = realloc(entries, ++count * sizeof *entries))) {
-      fprintf(stderr, "malloc failed");
-      return;
-    }
-
-    /* add entries to the array */
-    if (!(entries[count-1] = strdup(entry))) {
-      fprintf(stderr, "strdup failed");
-      return;
-    }
-  }
-
-  free(str);
+  return (strcmp(address1->email, address2->email));
 }
 
-void find_from(char *fn) {
-  FILE *file;
-  char line[1000];
-  char *entry;
+char *strtrim(char *str) {
+  char *pch = str;
 
-  file = fopen(fn, "r");
-  if (!file) {
-    fprintf(stderr, "%s: failed to open file.\n", fn);
-    return;
+  if (!str || *str == '\0') {
+    return(str);
   }
 
-  while ((fgets(line, 1000, file) != NULL)) {
-    if (strncmp("From: ", line, 6) == 0) {
-      /* trim leading "From: " and trailing "\n" */
-      entry = strndup(line + 6, strlen(line) - 1);
-      add_entries(entry);
-      free(entry);
-      break;
-    }
+  while (isspace((unsigned char)*pch)) {
+    pch++;
+  }
+  if (pch != str) {
+    memmove(str, pch, (strlen(pch) + 1));
   }
 
-  fclose(file);
+  if (*str == '\0') {
+    return(str);
+  }
+
+  pch = (str + (strlen(str) - 1));
+  while (isspace((unsigned char)*pch)) {
+    pch--;
+  }
+  *++pch = '\0';
+
+  return(str);
 }
 
-/* read a directory's contents and operate on each file */
-int read_from_dir(char *path) {
-  DIR *dir;
-  struct dirent *dit;
-
-  char *fullpath, *filename;
-
-  if ((dir = opendir(path)) == NULL) {
-    fprintf(stderr, "%s: unable to open directory.\n", path);
-    return(1);
+void add_address(address_t *address) {
+  if (!(entries = realloc(entries, ++entry_count * sizeof *entries))) {
+    fprintf(stderr, "realloc failed");
+    exit(EXIT_FAILURE);
   }
 
-  /* main loop */
-  while ((dit = readdir(dir)) != NULL) {
-    filename = dit->d_name;
+  entries[entry_count - 1] = address;
+}
 
-    /* skip . and .. */
-    if (strcmp(filename, ".") != 0 && strcmp(filename, "..") != 0) {
-      asprintf(&fullpath, "%s/%s", path, filename);
-      find_from(fullpath);
-      free(fullpath);
+address_t *parse_from(char *line) {
+  char name[64], email[64];
+  address_t *address;
+  int count;
+
+  count = sscanf(line, "%50[^<]<%50[^>]", name, email);
+  if (count != 2) {
+    return(NULL);
+  }
+
+  strtrim(name);
+  strtrim(email);
+
+  if (regexec(&regex, name, 0, 0, 0) == REG_NOMATCH &&
+      regexec(&regex, email, 0, 0, 0) == REG_NOMATCH) {
+    return(NULL);;
+  }
+
+  address = malloc(sizeof *address);
+  address->name = strdup(name);
+  address->email = strdup(email);
+
+  return(address);
+}
+
+int parse_mailfile(FILE *fp) {
+  char line[PATH_MAX];
+  address_t *entry;
+
+  while (fgets(line, PATH_MAX, fp) != NULL) {
+    if (strncmp(line, "From: ", 6) != 0) {
+      continue;
     }
-  }
 
-  if (closedir(dir) == -1) {
-    fprintf(stderr, "%s: unable to close directory.\n", path);
-    return(1);
+    entry = parse_from(&line[6]);
+    if (entry) {
+      add_address(entry);
+    }
   }
 
   return(0);
 }
 
-/* sorting/uniquing code bogarted from dmenu_path */
-int qstrcmp(const void *a, const void *b) {
-  return(strcmp(*(const char **)a, *(const char **)b));
+int read_maildir(const char *path, DIR *dirp) {
+  struct dirent *dentry;
+  char *filename;
+  FILE *fp;
+
+  while ((dentry = readdir(dirp)) != NULL) {
+    if (dentry->d_type != DT_REG) {
+      continue;
+    }
+
+    asprintf(&filename, "%s/%s", path, dentry->d_name);
+
+    fp = fopen(filename, "r");
+    if (!fp) {
+      fprintf(stderr, "%s: ", filename);
+      perror("fopen");
+      continue;
+    }
+
+    parse_mailfile(fp);
+    fclose(fp);
+  }
+
+  return(0);
 }
 
-void sort_and_print_entries(void) {
-  size_t i;
+void print_entries() {
+  int i;
+  char *prev_email = NULL;
+  address_t **entry;
 
-  qsort(entries, count, sizeof *entries, qstrcmp);
-
-  for (i = 0; i < count; i++) {
-    if (i > 0 && !strcmp(entries[i], entries[i-1]))
+  for (i = 0, entry = entries; i < entry_count; i++, entry++) {
+    if (prev_email && strcmp(prev_email, (*entry)->email) == 0) {
       continue;
+    }
 
-    printf("%s\n", entries[i]);
+    printf("%s\t%s\n", (*entry)->email, (*entry)->name);
+    prev_email = (*entry)->email;
   }
 }
 
 int main(int argc, char *argv[]) {
   char *path;
+  const char **subdir;
+  DIR *dirp;
   int i;
 
-  /* invalid usage */
   if (argc < 3) {
-    printf("usage: %s <search string> </path/to/mdir> ...\n", argv[0]);
-    return(1);
+    printf("usage: %s <regex> </path/to/mdir> ...\n", argv[0]);
+    return(EXIT_FAILURE);
   }
 
   if (regcomp(&regex, argv[1], REGEX_OPTS) != 0) {
@@ -136,29 +166,25 @@ int main(int argc, char *argv[]) {
   }
 
   for (i = 2; i < argc; i++) {
-    /* check the new subdir */
-    asprintf(&path, "%s/%s", argv[i], "new");
-
-    if (read_from_dir(path) == 1) {
-      fprintf(stderr, "%s: unable to read from path.\n", path);
+    for (subdir = subdirs; *subdir; subdir++) {
+      asprintf(&path, "%s/%s", argv[i], *subdir);
+      dirp = opendir(path);
+      if (!dirp) {
+        fprintf(stderr, "error: unable to read from path: %s: ", path);
+        perror("");
+        continue;
+      }
+      read_maildir(path, dirp);
+      free(path);
+      closedir(dirp);
     }
-
-    free(path);
-
-     /* check the cur subdir */
-    asprintf(&path, "%s/%s", argv[i], "cur");
-
-    if (read_from_dir(path) == 1) {
-      fprintf(stderr, "%s: unable to read from path.\n", path);
-    }
-
-    free(path);
   }
 
   regfree(&regex);
 
-  /* output */
-  printf("\n");
-  sort_and_print_entries();
-  return(0);
+  qsort(entries, entry_count, sizeof *entries, address_cmp); 
+  print_entries();
+
+  return(EXIT_SUCCESS);
 }
+
